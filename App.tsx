3272074b2c5eb0cb0f1c1c2d67c7ea15ef35515b
OscarDRT <oscarnetworkingpro@gmail.com>
2023-01-13 20:13:13 -0500

    feat: config rules

--- App.tsx
+++ App.tsx
@@ -1,15 +1,21 @@
-import React from 'react';
-import {View} from 'react-native';
-import {Horse, Heart, Cube} from 'phosphor-react-native';
+import React from 'react'
+import { View } from 'react-native'
+import { Horse, Heart, Cube } from 'phosphor-react-native'
 
 const App = () => {
   return (
-    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+    <View
+      style={{
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}
+    >
       <Horse />
       <Heart color="#AE2983" weight="fill" size={32} />
       <Cube color="teal" weight="duotone" />
     </View>
-  );
-};
+  )
+}
 
-export default App;
+export default App
--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,20 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
-};
+  env: {
+    production: {
+      plugins: ['transform-remove-console'],
+    },
+  },
+  plugins: [
+    [
+      'module-resolver',
+      {
+        alias: {
+          '@root': './src',
+          '@screens': './src/screens',
+          '@components': './src/components',
+        },
+      },
+    ],
+  ],
+}
